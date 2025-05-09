47fff6a5cecd010632673329b42eea013105598c
thomasSchi <thomas.schiller@4am-robotics.com>
2025-05-09 08:43:48 +0200

    First Commit

--- obj/Debug/net8.0/BlazorApptest2.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApptest2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApptest2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6e70779aa5789deab84a78738ad3a018036bfd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApptest2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApptest2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
